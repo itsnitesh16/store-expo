8854d49010f6aae55b682ff8be246dfe53427733
itsnitesh16 <niteshkumarghn1@gmail.com>
2026-01-14 22:55:24 +0530

    clerk, nativewind, tanstackQuery setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import "../global.css";
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{
+          headerShown: false
+        }} />
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
 }
