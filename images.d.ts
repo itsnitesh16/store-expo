1f18cf40426f9ee790f6d29310dc8c52a8e49be5
itsnitesh16 <niteshkumarghn1@gmail.com>
2026-01-15 00:03:30 +0530

    auth screen implemented

---
+++ images.d.ts
@@ -0,0 +1,20 @@
+declare module "*.png" {
+  const content: ImageSourcePropType
+  export default content;
+}
+declare module "*.jpg" {
+  const content: ImageSourcePropType;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: ImageSourcePropType;
+  export default content;
+}
+declare module "*.gif" {
+  const content: ImageSourcePropType;
+  export default content;
+}
+declare module "*.svg" {
+  const content: ImageSourcePropType;
+  export default content;
+}
